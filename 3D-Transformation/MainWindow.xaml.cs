d7b57d4feda57b1fa03d9ad6929d03e971c9cb46
aikbbnaur <aikbbnaur@gmail.com>
2018-04-28 21:42:42 +0700

    Translation GUI

--- 3D-Transformation/MainWindow.xaml.cs
+++ 3D-Transformation/MainWindow.xaml.cs
@@ -15,14 +15,13 @@ using System.Windows.Shapes;
 
 namespace _3D_Transformation
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
+
+
     }
 }
